aae650209d06b46fd6d97c2252e61e2ca891c861
Logan Staley <Logan170828@yahoo.com>
2023-09-27 00:51:07 -0500

    commit

--- Project/flutterproject_v1/lib/login.dart
+++ Project/flutterproject_v1/lib/login.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'widgets/inputtextbox.dart';
 import 'widgets/textonlybutton.dart';
-import 'createaccount.dart'; // Import the CreateAccountPage
+import 'createaccount.dart';
 
 class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> login(BuildContext context) async {
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2/your_project_folder/login.php'),
+      body: jsonEncode(<String, String>{
+        'username': usernameController.text,
+        'password': passwordController.text,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      if (data["success"]) {
+        // Handle successful login, e.g., navigate to another page
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(data["message"])),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to connect to the server.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,18 +44,22 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            InputTextBox(isPassword: false, hint: 'Username or Email'),
+            InputTextBox(controller: usernameController, isPassword: false, hint: 'Username or Email'),
             SizedBox(height: 20),
-            InputTextBox(isPassword: true, hint: 'Enter your password'),
+            InputTextBox(controller: passwordController, isPassword: true, hint: 'Enter your password'),
             SizedBox(height: 20),
+            TextOnlyButton(
+              text: 'Login',
+              onPressed: () {
+                login(context);
+              },
+            ),
             TextOnlyButton(
               text: 'Create Account',
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          CreateAccountPage()), // Navigate to CreateAccountPage
+                  MaterialPageRoute(builder: (context) => CreateAccountPage()),
                 );
               },
             ),
--- Project/flutterproject_v1/lib/widgets/inputtextbox.dart
+++ Project/flutterproject_v1/lib/widgets/inputtextbox.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class InputTextBox extends StatelessWidget {
   final bool isPassword;
   final String hint;
+  final TextEditingController controller; // Add this line
 
   // Define the width and height for the InputTextBox
   final double width = 300.0;
   final double height = 50.0;
 
-  InputTextBox({required this.isPassword, required this.hint});
+  InputTextBox({required this.isPassword, required this.hint, required this.controller}); // Modify this line
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +17,7 @@ class InputTextBox extends StatelessWidget {
       width: width,
       height: height,
       child: TextField(
+        controller: controller, // Add this line
         obscureText: isPassword, // If true, the text will be obscured with dots
         decoration: InputDecoration(
           border: OutlineInputBorder(),
