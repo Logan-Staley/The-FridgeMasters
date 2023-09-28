a72dbf0590c155f1ba95a63f2d2127f02d9b0fce
Freddie Munoz <munozf5338@uhcl.edu>
2023-09-28 00:39:30 -0500

    commit

--- Project/flutterproject_v1/lib/homepage.dart
+++ Project/flutterproject_v1/lib/homepage.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:fridgemasters/widgets/taskbar.dart';
+import 'package:fridgemasters/notificationlist.dart'; // Import NotificationList
+import 'package:fridgemasters/settings.dart'; // Import Settings
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      
-      body: Center(
-        child: Text('Welcome to the Home Page!'),
-        
-
-      
-
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.notifications), // Notifications icon
+          onPressed: () {
+            // Navigate to NotificationList page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NotificationList()),
+            );
+          },
+        ),
+        // No title here, so the center is empty
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to Settings page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Settings()),
+              );
+            },
+          ),
+        ],
       ),
-
-      bottomNavigationBar: Taskbar(currentIndex: 0, onTabChanged: (index){
-        
-      }
-      
-      
-      
-      
-      )
-       
+      body: Center(
+          // No text here, so the body center is empty
+          ),
+      bottomNavigationBar: Taskbar(
+        currentIndex: 0,
+        onTabChanged: (index) {
+          // Handle tab change
+        },
+      ),
     );
-
-    
   }
-   
-
-
-
 }
---
+++ Project/flutterproject_v1/lib/settings.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  String _selectedLanguage = 'English'; // Default selected language
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous page
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Language',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            RadioListTile<String>(
+              title: const Text('English'),
+              value: 'English',
+              groupValue: _selectedLanguage,
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedLanguage = value!;
+                });
+              },
+            ),
+            RadioListTile<String>(
+              title: const Text('Spanish'),
+              value: 'Spanish',
+              groupValue: _selectedLanguage,
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedLanguage = value!;
+                });
+              },
+            ),
+            Spacer(), // Pushes the Sign Out button to the end
+            ElevatedButton(
+              onPressed: () {
+                // Implement your sign-out logic here
+              },
+              child: Text('Sign Out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
