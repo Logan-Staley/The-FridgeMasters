edb600649ba8531eecb382a3f2027ddfae11f321
Logan Staley <logan170828@yahoo.com>
2023-09-28 12:16:17 -0500

    Revert "Logan s"

--- Project/flutterproject_v1/lib/login.dart
+++ Project/flutterproject_v1/lib/login.dart
@@ -1,9 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-
-
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:fridgemasters/homepage.dart';
 import 'package:fridgemasters/widgets/button.dart';
 import 'widgets/inputtextbox.dart';
@@ -11,39 +7,7 @@ import 'widgets/textonlybutton.dart';
 import 'createaccount.dart';
 import 'widgets/backgrounds.dart'; // Import the new Background1 widget
 
-
 class LoginPage extends StatelessWidget {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  Future<void> login(BuildContext context) async {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2/your_project_folder/login.php'),
-      body: jsonEncode(<String, String>{
-        'username': usernameController.text,
-        'password': passwordController.text,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
-      if (data["success"]) {
-        // Handle successful login, e.g., navigate to another page
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(data["message"])),
-        );
-      }
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to connect to the server.')),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,14 +20,13 @@ class LoginPage extends StatelessWidget {
               children: [
                 Image.asset('images/fridgemasters-logo.png'), // Logo Image
                 SizedBox(height: 20),
-                InputTextBox(controller: usernameController, isPassword: false, hint: 'Username or Email'),
+                InputTextBox(isPassword: false, hint: 'Username or Email'),
                 SizedBox(height: 20),
-                InputTextBox(controller: passwordController, isPassword: true, hint: 'Enter your password'),
+                InputTextBox(isPassword: true, hint: 'Enter your password'),
                 SizedBox(height: 20),
                 Button(
                   onPressed: () {
                     // Your login logic here
-                    login(context);
                   },
                   buttonText: 'Login',
                   nextPage: HomePage(),
@@ -80,7 +43,6 @@ class LoginPage extends StatelessWidget {
                   },
                 ),
               ],
-
             ),
           ),
         ],
--- Project/flutterproject_v1/lib/widgets/inputtextbox.dart
+++ Project/flutterproject_v1/lib/widgets/inputtextbox.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 class InputTextBox extends StatelessWidget {
   final bool isPassword;
   final String hint;
-  final TextEditingController controller; // Add this line
 
   // Define the width and height for the InputTextBox
   final double width = 300.0;
   final double height = 50.0;
 
-  InputTextBox({required this.isPassword, required this.hint, required this.controller}); // Modify this line
+  InputTextBox({required this.isPassword, required this.hint});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +25,6 @@ class InputTextBox extends StatelessWidget {
         borderRadius: BorderRadius.circular(4),
       ),
       child: TextField(
-        controller: controller, // Add this line
         obscureText: isPassword, // If true, the text will be obscured with dots
         decoration: InputDecoration(
           border: OutlineInputBorder(
