f5fae189010c43008ede9308cd138db4f7280e25
Freddie Munoz <munozf5338@uhcl.edu>
2023-09-27 21:30:25 -0500

    commit

--- Project/flutterproject_v1/lib/widgets/button.dart
+++ Project/flutterproject_v1/lib/widgets/button.dart
@@ -2,18 +2,25 @@ import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
   final VoidCallback onPressed;
+  final String buttonText;
+  final Widget nextPage; // Add this line to accept the next page
 
-  Button({required this.onPressed});
+  Button(
+      {required this.onPressed,
+      required this.buttonText,
+      required this.nextPage}); // Modify the constructor
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
-     child: Text(''),  // Text displayed on the button
+      onPressed: () {
+        onPressed();
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => nextPage),
+        );
+      },
+      child: Text(buttonText),
     );
   }
 }
-
-
-//This is a small button for login, save, cancel.
-// First Button used in Login: Changed text to login in the login.dart page
