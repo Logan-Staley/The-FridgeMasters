72202ef1939e2d8a1d589bea660593e61008a50e
Mireya Carino <mireyac05@yahoo.com>
2023-10-02 23:00:47 -0500

    commit

---
+++ Project/flutterproject_v1/lib/widgets/ForgotPassswordPage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:fridgemasters/widgets/inputtextbox.dart';
+
+
+class ForgotPasswordPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+
+  // This function simulates sending a password reset email
+  void sendPasswordResetEmail(BuildContext context) {
+    String email = emailController.text;
+    if (email.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your email.')),
+      );
+      return;
+    }
+    // In a real app, you'd try to send a password reset email here
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Password reset email sent to $email')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forgot Password'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: 'Enter your email',
+                hintText: 'example@example.com',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                sendPasswordResetEmail(context);
+              },
+              child: Text('Send Password Reset Email'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
