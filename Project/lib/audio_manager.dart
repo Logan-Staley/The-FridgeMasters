50b5f5e458519b0246ff90dc2ba942f7b2f5f4cb
Michael Ndudim <102204060+MichaelNdudim@users.noreply.github.com>
2023-10-20 21:12:32 -0500

    Create audio_manager.dart

---
+++ Project/lib/audio_manager.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class AudioManager with WidgetsBindingObserver {
+  static final AudioManager _instance = AudioManager._internal();
+  factory AudioManager() => _instance;
+  AudioManager._internal() {
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  Future<void> startBackgroundMusic() async {
+    await _audioPlayer.play('sounds/background_before_login.mp3', volume: 0.025, isLocal: false);
+  }
+
+  Future<void> stopBackgroundMusic() async {
+    await _audioPlayer.stop();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.inactive) {
+      stopBackgroundMusic();
+    } else if (state == AppLifecycleState.resumed) {
+      startBackgroundMusic();
+    }
+  }
+
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _audioPlayer.dispose();
+  }
+}
