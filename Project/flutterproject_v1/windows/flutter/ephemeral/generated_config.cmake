22ba319494ec9b51d98da0b8c92592c398c376db
Freddie Munoz <munozf5338@uhcl.edu>
2023-10-21 19:27:36 -0500

    commit

---
+++ Project/flutterproject_v1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Synology Drive Shared\\SynologyDrive\\School Stuff\\Senior Project\\Freddie\\Flutter Apps\\The-FridgeMasters\\Project\\flutterproject_v1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter"
+  "PROJECT_DIR=D:\\Synology Drive Shared\\SynologyDrive\\School Stuff\\Senior Project\\Freddie\\Flutter Apps\\The-FridgeMasters\\Project\\flutterproject_v1"
+  "FLUTTER_ROOT=D:\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Synology Drive Shared\\SynologyDrive\\School Stuff\\Senior Project\\Freddie\\Flutter Apps\\The-FridgeMasters\\Project\\flutterproject_v1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Synology Drive Shared\\SynologyDrive\\School Stuff\\Senior Project\\Freddie\\Flutter Apps\\The-FridgeMasters\\Project\\flutterproject_v1"
+  "FLUTTER_TARGET=D:\\Synology Drive Shared\\SynologyDrive\\School Stuff\\Senior Project\\Freddie\\Flutter Apps\\The-FridgeMasters\\Project\\flutterproject_v1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iOGQzNTgxMGU5MWFiOGZjMzliYTVlN2E0MWJmZjZmNjk3ZThlM2E4Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Synology Drive Shared\\SynologyDrive\\School Stuff\\Senior Project\\Freddie\\Flutter Apps\\The-FridgeMasters\\Project\\flutterproject_v1\\.dart_tool\\package_config.json"
+)
