b5c58758deaec3238a5290972ccc48fdf3607d61
Mireya Carino <mireyac05@yahoo.com>
2023-10-25 21:59:14 -0500

    commit

--- Project/lib/settings.dart
+++ Project/lib/settings.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fridgemasters/Aboutpage.dart';
+import 'package:fridgemasters/FAQpage.dart';
+import 'package:fridgemasters/System.dart';
+import 'package:fridgemasters/widgets/DisplayandBrightness.dart';
 import 'package:fridgemasters/widgets/backgrounds.dart'; 
 import 'package:fridgemasters/login.dart';
 import 'package:fridgemasters/widgets/account_settings.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:fridgemasters/audio_manager.dart';
-//import 'package:fridgemasters/FAQpage.dart';
-//import 'package:fridgemasters/Aboutpage.dart';
+import 'package:fridgemasters/Notificationspage.dart';
+
+
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -55,10 +60,23 @@ class _SettingsState extends State<Settings> {
     Widget? nextPage;
     switch (category) {
       case 'Account':
-        nextPage = AccountSettings();
-        break;
-      
-   
+      nextPage = AccountSettings();
+      break;
+      case 'Display and Brightness':
+      nextPage = DisplayandBrightness();
+      break;
+      case 'Notifications':
+      nextPage = Notificationspage();
+      break;
+      case 'System':
+      nextPage = System();
+      break;
+      case 'FAQs':
+      nextPage = FAQpage();
+      break;
+      case 'About':
+      nextPage = Aboutpage();
+      break;
    
       
       
---
+++ Project/lib/widgets/DisplayandBrightness.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DisplayandBrightness extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Display and Brightness'),
+      ),
+      body: Center(
+        child: Text('Content for Display and Brightness page'),
+      ),
+    );
+  }
+}
