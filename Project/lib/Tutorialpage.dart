b8c893537004df804daaff5ed2bd0583635d21ca
Logan Staley <logan170828@yahoo.com>
2023-11-13 11:30:11 -0600

    Revert "commit"
    This reverts commit 6326beeec801edde8df38ede42bb83bcb4aac30c.

--- Project/lib/Tutorialpage.dart
+++ Project/lib/Tutorialpage.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class TutorialPage extends StatelessWidget {
+class Tutorialpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tutorial Page'),
+        title: Text('system'),
       ),
       body: Center(
-        child: Text('Welcome to the Tutorial!'),
+        child: Text('Content system page'),
       ),
     );
   }
