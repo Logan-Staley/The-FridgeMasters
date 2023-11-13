5757135dde2e0170e48b8f4fa81a4600f89e2af7
Mireya Carino <mireyac05@yahoo.com>
2023-11-12 19:34:22 -0600

    commit

--- Project/lib/homepage.dart
+++ Project/lib/homepage.dart
@@ -59,8 +59,10 @@ class _ExpiringItemTileState extends State<ExpiringItemTile> with SingleTickerPr
   Widget _nonExpiringBorder(Widget child) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: Color.fromARGB(255, 20, 220, 27), width: 2.0),
+        border: Border.all(color: Color.fromARGB(255, 168, 169, 173), width: 0.8),
+         borderRadius: BorderRadius.circular(70), 
       ),
+      
       child: child,
     );
   }
@@ -92,10 +94,14 @@ class _ExpiringItemTileState extends State<ExpiringItemTile> with SingleTickerPr
         ).lerp(_animationController.value);
         return Container(
           decoration: BoxDecoration(
-            border: Border.all(color: color!, width: 2.0),
-          ),
-          child: child,
-        );
+      border: Border.all(
+        color: Color.fromARGB(255, 168, 169, 173),
+        width: 0.8,
+      ),
+      borderRadius: BorderRadius.circular(70), // Rounded corners
+    ),
+    child: child,
+  );
       },
     );
   }
@@ -193,16 +199,16 @@ class _HomePageState extends State<HomePage> {
                 .split(" ")[0], // Only take the date part, exclude the time
 
             'imageUrl':
-                item['imageUrl'], // Keep as default or adjust as necessary
-            'nutrients': {
+                item['imageUrl'] ,
+                 'nutrients': {
       'ENERC_KCAL': item['ENERC_KCAL'],
       'PROCNT': item['PROCNT'],
       'FAT': item['FAT'],
       'CHOCDF': item['CHOCDF'],
       'FIBTG': item['FIBTG'],
-    },
-  };
-}).toList();
+                  }, // Keep as default or adjust as necessary
+          };
+        }).toList();
 print('Formatted Items: $formattedItems'); // Print the formattedItems list
         setState(() {
           widget.fridgeItems.addAll(formattedItems);
@@ -274,7 +280,7 @@ Widget _nonExpiringBorder(Widget child) {
   );
 }
 
-  @override
+ /* @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -285,8 +291,63 @@ Widget _nonExpiringBorder(Widget child) {
               color: const Color.fromARGB(255, 215, 215, 215),
               width: 2), // Blue border
         ),
+         title: Column(
+          mainAxisSize: MainAxisSize.min, // Use min size for the column
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 35.0), // Adjust the padding to move the title down
+              child: Center(child: Text('The Fridge Masters')),
+            ),
+          ],
+        ), */
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: PreferredSize(
+      preferredSize: Size.fromHeight(140.0), // Adjust the height as needed
+      child: AppBar(
+        backgroundColor: Colors.transparent, // Make the AppBar background transparent
+        elevation: 0, // Removes the default shadow
+         shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Color.fromARGB(253, 253, 253, 253),
+            width: 2,
+          ),
+        ),
+       
+        flexibleSpace: Padding( // Apply padding to the flexibleSpace
+          padding: const EdgeInsets.symmetric(horizontal: 21.0), // Set horizontal padding
+          child: ClipRRect(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)), // Rounded corners at the bottom
+            child: Container(
+              margin: const EdgeInsets.only(top: 45.0), // Top margin to push AppBar down
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 168, 169, 173), // Your AppBar color
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 215, 215, 215),
+                  width: 2,
+                  
+                ),
+              ),
+            ),
+          ),
+        ),
+        title: Column(
+          mainAxisSize: MainAxisSize.min, // Use min size for the column
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 35.0), // Adjust the padding to move the title down
+              child: Center(child: Text('The Fridge Masters')),
+            ),
+          ],
+        ),
+
+        leading:  Transform.translate(
+        offset: Offset(11, 19), 
         
-        leading: IconButton(
+       child: IconButton(
+          
           icon: Icon(Icons.notifications),
           onPressed: () {
             Navigator.push(
@@ -297,8 +358,12 @@ Widget _nonExpiringBorder(Widget child) {
             );
           },
         ),
+        ),
         actions: [
-          IconButton(
+          Transform.translate(
+          offset: Offset(-11, 19),
+
+          child:IconButton(
             icon: Icon(Icons.settings),
             onPressed: () {
               Navigator.push(
@@ -308,29 +373,56 @@ Widget _nonExpiringBorder(Widget child) {
                 ),
               );
             },
-          ),
+          )),
         ],
+      
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(60),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            //padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            padding: const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 10.0),
+            child: Theme(
+      data: Theme.of(context).copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: Colors.white, // Changes the cursor and selection handle color
+        ),
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.white, // Changes the cursor color
+          selectionColor: Colors.white.withOpacity(0.5), // Changes the selection color
+          selectionHandleColor: Colors.white, // Changes the selection handle color
+        ),
+      ),
+
+
+
+
             child: TextField(
               controller: _searchController,
               decoration: InputDecoration(
-                hintText: 'Filter based on food items',
-                border: OutlineInputBorder(
-                  //backgroundColor: color.Yellow,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                prefixIcon: Icon(Icons.search),
+               hintText: 'Filter based on food items',
+          prefixIcon: Icon(Icons.search), // Icon for search
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white), // Choose border color
+            borderRadius: BorderRadius.circular(20), // Choose border radius
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white), // Changes the border color when the TextField is focused
+            borderRadius: BorderRadius.circular(20),
               ),
             ),
           ),
         ),
       ),
+      ),
+      ),
+    ),
       body: Stack(
         children: [
-          const Background(type: 'Background1'),
+          //const Background(type: 'Background1'),
           Center(
               child: widget.fridgeItems.isEmpty
                   ? Column(
@@ -375,7 +467,7 @@ Widget _nonExpiringBorder(Widget child) {
         ),
       ),
     ),
-    SizedBox(height: 5),
+   /* SizedBox(height: 5),
           Center(
             child: RichText(
               textAlign: TextAlign.center,
@@ -386,7 +478,7 @@ Widget _nonExpiringBorder(Widget child) {
                     style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.bold), 
                   ),
                   TextSpan(
-                    text: '🟢',
+                    text: '🟡',
                     style: TextStyle(color: Color.fromARGB(255, 4, 114, 8), fontSize: 17, fontWeight: FontWeight.bold), 
                   ),
                   TextSpan(
@@ -402,7 +494,7 @@ Widget _nonExpiringBorder(Widget child) {
                     style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 16, fontWeight: FontWeight.bold), 
                   ),
                   TextSpan(
-                    text: '🔴',
+                    text: '🟡',
                     style: TextStyle(color: Color.fromARGB(255, 226, 50, 50), fontSize: 17, fontWeight: FontWeight.bold),
                   ),
                   TextSpan(
@@ -412,7 +504,7 @@ Widget _nonExpiringBorder(Widget child) {
                 ],
               ),
             ),
-          ),
+          ),*/
           SizedBox(height: 10),
         ],
       );
@@ -420,14 +512,20 @@ Widget _nonExpiringBorder(Widget child) {
       // Index is greater than 0, so it's an item
                         final item = widget.fridgeItems[index-1];
                         final itemId = item['itemId'];
-                        //print('Item ID at index $index: $itemId');
-                        Color _getPastelColor(int index) {
+                        print('Item ID at index $index: $itemId');
+
+                           Color _getLightGrayColor() {
+  // Light gray color #F0F0F0
+                         return Color.fromRGBO(240, 240, 240, 1); // Opacity is set to 1 for a solid color
+}
+
+                        /*Color _getPastelColor(int index) {
                           final r = (70 + (index * 50) % 135).toDouble();
                           final g = (90 + (index * 80) % 85).toDouble();
                           final b = (120 + (index * 30) % 55).toDouble();
                           return Color.fromRGBO(
                               r.toInt(), g.toInt(), b.toInt(), 0.9);
-                        }
+                        }*/
 
                      ImageProvider getImageProvider(String? imageUrl) {
   // Check if imageUrl is a network URL
@@ -442,8 +540,21 @@ Widget _nonExpiringBorder(Widget child) {
                            return Padding(
   padding: const EdgeInsets.all(8.0),
   child: Card(
-    color: _getPastelColor(index),
-    elevation: 4.0, // Added shadow
+    color: _getLightGrayColor(),
+    
+    //color: _getPastelColor(index),
+    elevation: 4.0,
+   shape: RoundedRectangleBorder(
+  side: BorderSide(
+    color: Color.fromARGB(255, 168, 169, 173), // Same color
+    width: 0.8, // Same width
+  ),
+  borderRadius: BorderRadius.circular(70), // Same rounded corners as in _expiredBorder
+),
+
+
+
+   
     child:  ExpiringItemTile(
       expirationDate: item['expirationDate'],
       purchaseDate: item['purchaseDate'],
@@ -461,43 +572,43 @@ Widget _nonExpiringBorder(Widget child) {
     padding: const EdgeInsets.all(16.0), // Image padding
     child: GestureDetector(
       onTap: () {
-        // Action to perform on tap goes here.
-        // For instance, navigate to a new screen with the nutritional facts.
-        Navigator.push(
+         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => NutritionPage(item: item), // itemData should include 'name' and 'nutrients'
           ),
         );
       },
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.brown, width: 3),
-        ),
-        width: 100,
-        height: 100,
-        child: Image.network(
-          item['imageUrl'].toString() ?? '',
-          fit: BoxFit.cover,
-          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-            // In case of an error (like a 404), use the default image
-            return Image.asset('images/default_image.png', fit: BoxFit.cover);
-          },
-          loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-            if (loadingProgress == null) return child;
-            return Center(
-              child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                    : null,
-              ),
-            );
-          },
-        ),
+
+
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color.fromARGB(255, 197, 193, 191), width: 3),
+      ),
+      width: 100,
+      height: 100,
+      child: Image.network(
+        item['imageUrl'].toString() ?? '',
+        fit: BoxFit.cover,
+        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+          // In case of an error (like a 404), use the default image
+          return Image.asset('images/default_image.png', fit: BoxFit.cover);
+        },
+        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Center(
+            child: CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                  : null,
+            ),
+          );
+        },
       ),
     ),
   ),
 ),
+                                      ),
                                       Expanded(
                                         flex: 7,
                                         child: Column(
@@ -532,12 +643,13 @@ Widget _nonExpiringBorder(Widget child) {
                                                                             .bold,
                                                                     fontSize:
                                                                         18,
-                                                                    color: Color
+                                                                        color: Colors.black,
+                                                                   /* color: Color
                                                                         .fromARGB(
                                                                             255,
                                                                             255,
                                                                             255,
-                                                                            255),decoration: TextDecoration.underline)), // User-entered text size
+                                                                            255),decoration: TextDecoration.underline*/)), // User-entered text size
                                                           ],
                                                         ),
                                                       ),
@@ -568,12 +680,12 @@ Widget _nonExpiringBorder(Widget child) {
                                                                             .bold,
                                                                     fontSize:
                                                                         16,
-                                                                    color: Color
+                                                                    /*color: Color
                                                                         .fromARGB(
                                                                             255,
                                                                             255,
                                                                             255,
-                                                                            255))),
+                                                                            255)*/)),
                                                           ],
                                                         ),
                                                       ),
@@ -608,12 +720,13 @@ Widget _nonExpiringBorder(Widget child) {
                                                                             .bold,
                                                                     fontSize:
                                                                         15.5,
-                                                                    color: Color
+                                                                         color: Colors.black,
+                                                                    /*color: Color
                                                                         .fromARGB(
                                                                             255,
                                                                             255,
                                                                             255,
-                                                                            255))),
+                                                                            255)*/)),
                                                           ],
                                                         ),
                                                       ),
@@ -660,7 +773,7 @@ Widget _nonExpiringBorder(Widget child) {
                                       ),
                                     ],
                                   ),
-                                  Positioned(
+                                  /*Positioned(
                                     bottom: 1, // adjust as needed
                                     left: 38, // adjust as needed
                                     child: Text(
@@ -671,12 +784,12 @@ Widget _nonExpiringBorder(Widget child) {
                                             255), // or any color you prefer
                                       ),
                                     ),
-                                  ),
+                                  ),*/
                                    Positioned(
-                                    top: -5,
-                                    right: -5,
+                                    top: 2,
+                                    right: 2,
                                     child: IconButton(
-                                      icon: Icon(Icons.delete, size: 18),
+                                      icon: Icon(Icons.delete, size: 20),
                                       onPressed: () {
                                         showDialog(
                                           context: context,
@@ -766,7 +879,7 @@ Widget _nonExpiringBorder(Widget child) {
       floatingActionButton: FloatingActionButton(
         onPressed: _navigateToAddItem,
         child: Icon(Icons.add),
-        backgroundColor: const Color.fromARGB(210, 33, 149, 243),
+        backgroundColor: const Color.fromARGB(210, 84, 85, 87),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
@@ -774,3 +887,4 @@ Widget _nonExpiringBorder(Widget child) {
 }
 
 
+//check
