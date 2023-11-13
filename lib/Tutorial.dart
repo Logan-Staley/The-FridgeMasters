a50d7bb435645c47c93731d9b41dfa00581fb5b0
SirayT <144485855+SirayT@users.noreply.github.com>
2023-11-12 20:32:37 -0600

    Add files via upload

---
+++ lib/Tutorial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tutorialpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('system'),
+      ),
+      body: Center(
+        child: Text('Content system page'),
+      ),
+    );
+  }
+}
