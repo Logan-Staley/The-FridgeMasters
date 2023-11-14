9241e3a7314dfa614ae88bbd3dbf1cec2204ad54
SirayT <tmassiray@yahoo.com>
2023-11-13 22:48:03 -0600

    commit

--- Project/lib/Tutorialpage.dart
+++ Project/lib/Tutorialpage.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 
 class TutorialPage extends StatelessWidget {
   @override
@@ -7,9 +7,97 @@ class TutorialPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Tutorial Page'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Welcome to the Tutorial!'),
       ),
+    );
+  }
+} */
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:io';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: VideoAndTutorialPage(),
+    );
+  }
+}
+
+class VideoAndTutorialPage extends StatefulWidget {
+  @override
+  _VideoAndTutorialPageState createState() => _VideoAndTutorialPageState();
+}
+
+class _VideoAndTutorialPageState extends State<VideoAndTutorialPage> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(
+      File(
+        'C:\\Users\\tmass\\OneDrive - University of Houston-Clear Lake\\Documents\\UHCL\\Fall 2023 Classes\\ITEC 4388 (Senior Project)\\FridgeMasters2\\your_video_filename.mp4',
+      ),
+    )..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video and Tutorial Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Video Player
+          _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+          SizedBox(height: 20), // Adjust spacing as needed
+
+          // Tutorial Page
+          TutorialPage(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
+
+class TutorialPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Welcome to the Tutorial!'),
     );
   }
 }
