b6c4726759fafea57be150b09ed47e207c49579c
Mireya Carino <mireyac05@yahoo.com>
2023-11-21 00:12:43 -0600

    Changes googlefonts dependecy, changed title, add package

--- Project/lib/homepage.dart
+++ Project/lib/homepage.dart
@@ -1,5 +1,6 @@
 //import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:fridgemasters/inventory.dart';
 import 'package:fridgemasters/widgets/taskbar.dart';
 import 'package:fridgemasters/widgets/backgrounds.dart';
